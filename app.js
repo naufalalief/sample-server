a74787c299006f179bddc76d0cc8ae57db108053
Naufal Alief <naufalalief087@gmail.com>
2023-11-11 16:40:17 +0700

    x

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
+
+let swaggerDocument;
+try {
+  const file = fs.readFileSync("./docs/api-docs.yaml", "utf8");
+  swaggerDocument = YAML.parse(file);
+} catch (err) {
+  console.error("Failed to load api-docs.yaml:", err);
+}
+
 const port = process.env.PORT || 3000;
+
 const cors = require("cors");
+
 const indexroute = require("./routes");
+
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("selamat datang di server todo sequelize");
-});
+if (swaggerDocument) {
+  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+// app.get("/", (req, res) => {
+//   res.send("selamat datang di server todo sequelize");
+// });
+
 app.use(express.json());
 app.use(indexroute);
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
