963fd442be7c63adc7d08033540d021db13654ec
Naufal Alief <naufalalief087@gmail.com>
2023-11-13 09:53:27 +0700

    x

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -76,7 +76,12 @@ module.exports = {
       );
       res.status(200).json({
         message: "data updated successfully",
-        data: data,
+        data: {
+          id,
+          name,
+          isdone,
+          id_user,
+        },
       });
     } catch (error) {
       console.log(error);
